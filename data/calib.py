13035b4d1b436b34f62b9dd1534769ce7c017c81
RenS-6486 <148059389+RenS-6486@users.noreply.github.com>
2024-09-06 11:39:01 -0700

    Update calib.py

--- data/calib.py
+++ data/calib.py
@@ -9,10 +9,5 @@ def read_calib_matrices(filename_calib, resample_factor):
     with open(filename_calib, mode='r') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')  
         i = 0
-        for row in csv_reader:
-            print(row)
-            if i not in [0, 5, 10, 11]:
-                #tform_calib[i,:] = (list(map(float,row)))
-                i += 1
         raise ValueError('A very specific bad thing happened.')
     return tform_calib[4:8,:] @ tform_calib[0:4,:] @ np.array([[resample_factor,0,0,0], [0,resample_factor,0,0], [0,0,1,0], [0,0,0,1]], np.float32)
